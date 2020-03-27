38074da7d8d902777e49b722e0dd6e5a6eeff892
Marcello Herreshoff <marcello.herreshoff@gmail.com>
2020-03-26 17:10:27 -0700

    Added assumptions and computed initial conditions.

--- sir_model.py
+++ sir_model.py
@@ -5,6 +5,13 @@ import pickle
 
 from common import *
 
+
+# Assumptions:
+DAYS_INFECTION_TO_DEATH = 17
+AVERAGE_DEATH_RATE = 0.01
+
+
+# Load the data:
 places = pickle.load(open('time_series.pkl', 'rb'))
 
 def csv_as_matrix(path):
@@ -16,6 +23,21 @@ def parse_int(s):
 population = {(r[0], r[1], '') : parse_int(r[3])
         for r in csv_as_matrix('data_population.csv')}
 
+
+# Start calculating:
 for k in sorted(population.keys()):
-    if k in places:
-        print(k)
+    if k not in places: continue
+    # Compute y0, the initial conditions:
+    ts = places[k]
+    latest_date = ts.dates[-1]
+    latest_deaths = ts.deaths[-1]
+    start_day = latest_date - datetime.timedelta(DAYS_INFECTION_TO_DEATH)
+    start_day_I = latest_deaths / AVERAGE_DEATH_RATE
+    start_day_idx = ts.dates.index(start_day)
+    start_day_deaths = ts.deaths[start_day_idx]
+    start_day_R = start_day_deaths / AVERAGE_DEATH_RATE
+    start_day_S = population[k] - start_day_I - start_day_R
+
+    y0 = start_day_S, start_day_I, start_day_R
+    print("place=" + ','.join(k), " d=" + start_day.isoformat(),
+            "S=",start_day_S,"I=",start_day_I,"R=",start_day_R)
