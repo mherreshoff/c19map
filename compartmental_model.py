25e3adb4777aaf3f95793fbf117cb40ec9a72b6d
Marcello Herreshoff <marcello.herreshoff@gmail.com>
2020-04-05 14:24:05 -0700

    Docs.  Dropped ICU state.  Better cacher. Delete graphs before regenerating.

--- compartmental_model.py
+++ compartmental_model.py
@@ -3,43 +3,48 @@ import collections
 import csv
 import datetime
 import dateutil.parser
+import functools
 import matplotlib.pyplot as plt
 import numpy as np
 import os
 import pickle
 from scipy.integrate import odeint
 import scipy
+import shutil
 import sympy as sp
 import sys
 
 from common import *
 
 
-# ASSUMPTIONS:
+# Model parameters:
 LATENT_PERIOD = 3.5
-    # 1/sigma
+    # 1/sigma - The average length of time between contracting the disease
+    #     and becoming infectious.
     # Citation: 3 studies in the Midas Database with fairly close agreement.
 INFECTIOUS_PERIOD = 4
-P_SEVERE = 0.10
-P_CRITICAL = 0.30
-P_FATAL = 0.35
-HOSPITAL_DURATION = 4
-ICU_DURATION = 14
+    # 1/gamma - The average length of time a person stays infectious
+    # TODO: get citation from Brandon.
+P_HOSPITAL = 0.10
+    # Probability an infectious case gets hospitalized
+HOSPITAL_DURATION = 17
+    # Average length of hospital stay.
+P_DEATH = 0.35
+    # Probability of death given hospitaliation
 
-# Growth rates, used to calculate betas:
+# Growth rates, used to calculate beta(t):
+# TODO: change these to have a leading one, simplifying a lot of other stuff.
 INTERVENTION_INFECTION_GROWTH_RATE = {
         'Unknown': 0.24,
         'No Intervention': 0.24,
         'Lockdown': 0.075,
         '~Lockdown': 0.1375,
         'Social Distancing': 0.2}
-  # Same question when there are active interventions.
 
 tuned_countries = set(['China', 'Japan', 'Korea, South'])
 
-_seir_beta_to_gr_cache = {}
+@functools.lru_cache(maxsize=10000)
 def seir_beta_to_growth_rate(beta):
-    if beta in _seir_beta_to_gr_cache: return _seir_beta_to_gr_cache[beta]
     sigma = 1/LATENT_PERIOD
     gamma = 1/INFECTIOUS_PERIOD
     m = np.array([
@@ -48,13 +53,11 @@ def seir_beta_to_growth_rate(beta):
     # Note: this matrix is the linearized version of the SEIR diffeq
     # where S~N for just E and I.
     w, v = np.linalg.eig(m)
-    result = np.exp(np.max(w)) - 1
-    _seir_beta_to_gr_cache[beta] = result
-    return result
+    return np.exp(np.max(w)) - 1
 
-_seir_gr_to_beta_cache = {}
+
+@functools.lru_cache(maxsize=10000)
 def seir_growth_rate_to_beta(igr):
-    if igr in _seir_gr_to_beta_cache: return _seir_gr_to_beta_cache[igr]
     sigma = 1/LATENT_PERIOD
     gamma = 1/INFECTIOUS_PERIOD
     beta = sp.Symbol('beta')
@@ -68,30 +71,27 @@ def seir_growth_rate_to_beta(igr):
     for eigenval in eigenvals:
         solns += sp.solvers.solve(eigenval-target_eigenval, beta)
     assert len(solns) == 1
-    result, = solns
-    _seir_gr_to_beta_cache[igr] = result
-    return result
+    return solns[0]
+
 
 INTERVENTION_BETA = {n : seir_growth_rate_to_beta(igr)
         for n, igr in INTERVENTION_INFECTION_GROWTH_RATE.items()} 
 
 
 class Model:
-    variables = "SEIHCDR"
+    variables = "SEIHDR"
     var_to_id = {v: i for i, v in enumerate(variables)}
     # The SEIHCDR Model.
     # An extension of the SEIR model.
     # See 'About' tab for: https://neherlab.org/covid19/
     def __init__(self,
             contact_rate, latent_t, infectious_t,
-            hospital_p, hospital_t, critical_p, critical_t, death_p):
+            hospital_p, hospital_t, death_p):
         self.contact_rate = contact_rate
         self.latent_t = latent_t
         self.infectious_t = infectious_t
         self.hospital_p = hospital_p
         self.hospital_t = hospital_t
-        self.critical_p = critical_p
-        self.critical_t = critical_t
         self.death_p = death_p
 
     def companion_matrix(self, t=0):
@@ -101,10 +101,8 @@ class Model:
             ('E','I', 'E', 1/self.latent_t),
             ('I','H', 'I', (1/self.infectious_t)*self.hospital_p),
             ('I','R', 'I', (1/self.infectious_t)*(1-self.hospital_p)),
-            ('H','C', 'H', (1/self.hospital_t)*model.critical_p),
-            ('H','R', 'H', (1/self.hospital_t)*(1-self.critical_p)),
-            ('C','D', 'C', (1/self.critical_t)*self.death_p),
-            ('C','R', 'C', (1/self.critical_t)*(1-self.death_p))]
+            ('H','D', 'H', (1/self.hospital_t)*model.death_p),
+            ('H','R', 'H', (1/self.hospital_t)*(1-self.death_p))]
         nv = len(Model.variables)
         m = np.zeros((nv, nv))
         for sv,tv,av,x in flows:
@@ -130,17 +128,15 @@ class Model:
         return growth_rate, state
 
     def derivative(self, y, t):
-        S,E,I,H,C,D,R = y
+        S,E,I,H,D,R = y
         N = np.sum(y) - D
         flows = [  # (from variable, to variable, rate)
             ('S','E', self.contact_rate(t) * I * S / N),
             ('E','I', E/self.latent_t),
             ('I','H', (I/self.infectious_t)*self.hospital_p),
             ('I','R', (I/self.infectious_t)*(1-self.hospital_p)),
-            ('H','C', (H/self.hospital_t)*model.critical_p),
-            ('H','R', (H/self.hospital_t)*(1-self.critical_p)),
-            ('C','D', (C/self.critical_t)*self.death_p),
-            ('C','R', (C/self.critical_t)*(1-self.death_p))]
+            ('H','D', (H/self.hospital_t)*model.death_p),
+            ('H','R', (H/self.hospital_t)*(1-self.death_p))]
         inbound = np.array([
             sum(x for s,t,x in flows if t==v) for v in Model.variables])
         outbound = np.array([
@@ -158,26 +154,26 @@ population = {(r[0], r[1], '') : parse_int(r[3])
         for r in csv_as_matrix('data_population.csv')}
 
 
-def default_beta(t):
-    return INTERVENTION_BETA['No Intervention']
-
 def interventions_to_beta(iv_dates, iv_strings, zero_day,
         growth_rate_power=None):
     # Takes a list of interventions.
     # Returns a function that computes beta from t.
-    if len(iv_dates) == 0: return default_beta
+    if growth_rate_power is None:
+        betas = INTERVENTION_BETA
+    else:
+        betas = {}
+        for k, b in INTERVENTION_BETA.items():
+            gr = seir_beta_to_growth_rate(b)
+            gr = (1 + gr) ** growth_rate_power - 1
+            betas[k] = seir_growth_rate_to_beta(gr)
+
+    if len(iv_dates) == 0:
+        return lambda t:betas['No Intervention']
     iv_offsets = [(d-zero_day).days for d in iv_dates]
     lowest = min(iv_offsets)
     highest = max(iv_offsets)
-    if growth_rate_power is None:
-        t_to_beta = {t: INTERVENTION_BETA[s]
-                for t, s in zip(iv_offsets, iv_strings)}
-    else:
-        t_to_beta = {}
-        for t, s in zip(iv_offsets, iv_strings):
-            gr = seir_beta_to_growth_rate(INTERVENTION_BETA[s])
-            gr = (1 + gr) ** growth_rate_power - 1
-            t_to_beta[t] = seir_growth_rate_to_beta(gr)
+    t_to_beta = {t: betas[s]
+            for t, s in zip(iv_offsets, iv_strings)}
     def beta(t):
         t = int(np.floor(t))
         if t in t_to_beta:
@@ -189,12 +185,11 @@ def interventions_to_beta(iv_dates, iv_strings, zero_day,
 
 # Set up a default version of the model:
 model = Model(
-        default_beta,
+        None,
         LATENT_PERIOD,
         INFECTIOUS_PERIOD,
-        P_SEVERE, HOSPITAL_DURATION,
-        P_CRITICAL, ICU_DURATION,
-        P_FATAL)
+        P_HOSPITAL, HOSPITAL_DURATION,
+        P_DEATH)
 
 
 # Outputs:
@@ -208,23 +203,25 @@ comparison_w.writerow(
         ["Province/State", "Country/Region", 
         "Deaths Predicted", "Deaths Actual"])
 
-if not os.path.exists('graphs'): os.makedirs('graphs')
-graph_days_forecast = 60
+# TODO: add flag for whether graphs happen.
+graph_output_dir = 'graphs'
+if os.path.exists(graph_output_dir):
+    shutil.rmtree(graph_output_dir)
+os.makedirs(graph_output_dir)
+
+graph_days_forecast = 60  #TODO: flag.
   # How many days into the future do we simulate?
 
 
 # Run the model forward for each of the places:
 for k in sorted(places.keys()):
     if k not in population: continue
+
     ts = places[k]
+    present_date = ts.dates[-1]
     N = population[k]
     place_s = ' - '.join([s for s in k if s != ''])
     print("Place =",place_s)
-
-    # Make deaths monotone:
-    for i in range(len(ts.deaths)-1):
-        if ts.deaths[i+1]<ts.deaths[i]:
-            ts.deaths[i+1] = ts.deaths[i]
 
     # We find a region starting where deaths are recorded and ending where
     # an intervention happens to do our curve fit with.
@@ -235,7 +232,6 @@ for k in sorted(places.keys()):
         continue
     start_idx = nz_deaths[0]
     fit_length = len(ts.dates)-start_idx
-    present_date = ts.dates[-1]
     model.contact_rate = interventions_to_beta(
             ts.intervention_dates, ts.interventions, present_date)
     growth_rate, equilibrium_state = model.equilibrium(t=-(fit_length-1))
@@ -246,7 +242,7 @@ for k in sorted(places.keys()):
     y0[0] = (10**9) - np.sum(y0)
 
     trajectories = odeint(lambda *a: model.derivative(*a), y0, t)
-    S, E, I, H, C, D, R = trajectories.T
+    S, E, I, H, D, R = trajectories.T
     # TODO: cut off when S < .9*N for accuracy in situations 
 
     if k[0] in tuned_countries:
@@ -271,7 +267,7 @@ for k in sorted(places.keys()):
     y0[0] = N - np.sum(y0)
 
     trajectories = odeint(lambda *a: model.derivative(*a), y0, t)
-    S, E, I, H, C, D, R = trajectories.T
+    S, E, I, H, D, R = trajectories.T
 
     #for k, v in sorted(info.items()):
     #    print("\t", k, ": ", v)
