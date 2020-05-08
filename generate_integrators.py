78b54e5118b09f16caaff327a5bcaf51d36d8c68
Marcello Herreshoff <marcello.herreshoff@gmail.com>
2020-05-08 02:42:59 -0700

    Cleanup of generate_integrators.

--- generate_integrators.py
+++ generate_integrators.py
@@ -25,106 +25,109 @@ floatX = theano.config.floatX
 cython_output.write(cython_header)
 
 
+# Note: an 'inflence' is a time-dependent parameter with values determined by linear interpolation.
+# Time-dependent parameters are changing external factors influencing the system.
 def ode_to_cython(
-        ode_name,
-        ode_variables,
-        ode_derivatives,
-        ode_fixed_parameters=None,
-        ode_interpolated_parameters=None):
-    if ode_fixed_parameters is None: ode_fixed_parameters = []
-    if ode_interpolated_parameters is None: ode_interpolated_parameters = []
+        name,
+        variables,
+        derivatives,
+        parameters=None,
+        influences=None):
+    if parameters is None: parameters = []
+    if influences is None: influences = []
+
+    if set(derivatives.keys()) != set(variables):
+        raise ValueError("Every variable must have a derivative, and vise versa.")
+
 
     symbols = {}
-    for v in ode_variables: symbols[v] = sp.Symbol(v)
-    for v in ode_fixed_parameters: symbols[v] = sp.Symbol(v)
-    for v in ode_interpolated_parameters: symbols[v] = sp.Symbol(v)
+    for s in variables + parameters + influences: symbols[s] = sp.Symbol(s)
+    dydt = {k : parse_expr(v, local_dict=symbols) for k,v in derivatives.items()}
 
-    ode_derivatives = {k : parse_expr(v, local_dict=symbols)
-            for k,v in ode_derivatives.items()}
+    ddy_dydt = [[sp.diff(dydt[v], sp.Symbol(w)) for w in variables] for v in variables]
+    ddp_dydt = [[sp.diff(dydt[v], sp.Symbol(p)) for p in parameters] for v in variables]
+    ddi_dydt = [[sp.diff(dydt[v], sp.Symbol(i)) for i in influences] for v in variables]
 
-    ddy_dydt = [[sp.diff(ode_derivatives[v], sp.Symbol(v2)) for v2 in ode_variables]
-        for v in ode_variables]
-
-    ddfp_dydt = [[sp.diff(ode_derivatives[v], sp.Symbol(fp)) for fp in ode_fixed_parameters]
-            for v in ode_variables]
-    ddip_dydt = [[sp.diff(ode_derivatives[v], sp.Symbol(ip)) for ip in ode_interpolated_parameters]
-            for v in ode_variables]
+    num_vars = len(variables)
+    num_params = len(parameters)
+    num_vp = len(variables) + len(parameters)
 
     def add(a):
         a = list(a)
         if len(a) == 0: return "0"
         return ' + '.join(a)
 
-    num_vars = len(ode_variables)
-    num_fparams = len(ode_fixed_parameters)
-    num_iparams = add(f"len({p}_vals)" for p in ode_interpolated_parameters)
-    if num_iparams == '': num_iparams = '0'
+    num_interpolation_points = add(f"len({i}_vals)" for i in influences)
 
-    def iparams_before(curr_p):
+    def interpolation_points_before(i):
         terms = []
-        for p in ode_interpolated_parameters:
-            if p == curr_p: break
-            terms.append(f"len({p}_vals)")
+        for j in influences:
+            if i == j: break
+            terms.append(f"len({j}_vals)")
         return add(terms)
 
-    vals_ip_vars = "".join(f"{ip}_vals, " for ip in ode_interpolated_parameters)
-    all_ip_variables = "".join(f"self.{ip}_ts, {ip}_vals, " for ip in ode_interpolated_parameters)
-    ip_ts_lens = ",".join(f"len({ip}_ts)" for ip in ode_interpolated_parameters)
+    influences_vals_vars = "".join(f"{i}_vals, " for i in influences)
+    all_influences_vars = "".join(f"self.{i}_ts, {i}_vals, " for i in influences)
+    influences_len_list = ",".join(f"len({i}_ts)" for i in influences)
 
     argument_list = [
         "np.ndarray[DTYPE_t, ndim=1] y0",
         "np.ndarray[DTYPE_t, ndim=1] params"]
-    for p in ode_interpolated_parameters:
+    for i in influences:
         argument_list += [
-            f"np.ndarray[DTYPE_t, ndim=1] {p}_ts",
-            f"np.ndarray[DTYPE_t, ndim=1] {p}_vals"]
+            f"np.ndarray[DTYPE_t, ndim=1] {i}_ts",
+            f"np.ndarray[DTYPE_t, ndim=1] {i}_vals"]
     argument_list += [
         "np.ndarray[DTYPE_t, ndim=1] ts",
         "float step"]
     arguments = "\n"+ " "*8 + (",\n" + " "*8).join(argument_list)
     integrator_code = simple_templates.expand("""
 # {'='*75}
-# Integrators for {ode_name}
+# Integrators for {name}
 
-{ode_name}_num_states = {num_vars}
-{ode_name}_num_fixed_params = {num_fparams}
+{name}_variables = {repr(variables)}
+{name}_parameters = {repr(parameters)}
+{name}_influences = {repr(influences)}
+{name}_derivatives = {repr(derivatives)}
+{name}_num_variables = len({name}_variables)
+{name}_num_parameters = len({name}_parameters)
+{name}_num_influences = len({name}_influences)
 %for include_sensitivity in [False, True]
 
 
-def {ode_name}_integrate{"_with_sensitivity" if include_sensitivity else ""}(
+def {name}_integrate{"_with_sensitivity" if include_sensitivity else ""}(
         %for a in argument_list
         {a},
         %end
         ):
-    %for i,v in enumerate(ode_variables)
+    %for i,v in enumerate(variables)
     cdef float {v} = y0[{i}]
     %end
-    %for i,p in enumerate(ode_fixed_parameters)
+    %for i,p in enumerate(parameters)
     cdef float {p} = params[{i}]
     %end
-    %for p in ode_interpolated_parameters
-    cdef int {p}_offset = {num_vars+num_fparams}+{iparams_before(p)}
-    cdef int {p}_idx = -1
-    cdef int {p}_idx_max = len({p}_ts)-1
-    cdef float {p}_frac
-    cdef float {p}
+    %for i in influences
+    cdef int {i}_offset = {num_vp}+{interpolation_points_before(i)}
+    cdef int {i}_idx = -1
+    cdef int {i}_idx_max = len({i}_ts)-1
+    cdef float {i}_frac
+    cdef float {i}
     %end
-    %for i,v in enumerate(ode_variables)
+    %for v in variables
     cdef float ddt_{v}
     %end
     cdef int t_idx = 0
     cdef int t_idx_max = len(ts)-1
     cdef float t = ts[0]
-    cdef int param_count = {num_vars+num_fparams}+{num_iparams}
+    cdef int param_count = {num_vp}+{num_interpolation_points}
     cdef np.ndarray[DTYPE_t, ndim=2] trajectory = np.zeros((len(ts),{num_vars}), dtype=DTYPE)
 %if include_sensitivity
-    cdef np.ndarray[DTYPE_t, ndim=2] dydp = np.zeros(
-        ({num_vars}, {num_vars+num_fparams}+{num_iparams}), dtype=DTYPE)
+    cdef np.ndarray[DTYPE_t, ndim=2] dydp = np.zeros(({num_vars}, param_count), dtype=DTYPE)
     cdef np.ndarray[DTYPE_t, ndim=2] ddt_dydp = np.zeros(({num_vars}, param_count), dtype=DTYPE)
     cdef np.ndarray[DTYPE_t, ndim=3] sensitivity = np.zeros(
         (len(ts),{num_vars},param_count), dtype=DTYPE)
     cdef float ddy_dydt_val
-    cdef float ddip_dydt_val
+    cdef float ddi_dydt_val
 
     for v in range({num_vars}):
         dydp[v,v] = 1
@@ -132,7 +135,7 @@ def {ode_name}_integrate{"_with_sensitivity" if include_sensitivity else ""}(
 
     while True:
         while t >= ts[t_idx]:
-            %for i,v in enumerate(ode_variables)
+            %for i,v in enumerate(variables)
             trajectory[t_idx, {i}] = {v}
             %end
             %if include_sensitivity
@@ -143,19 +146,19 @@ def {ode_name}_integrate{"_with_sensitivity" if include_sensitivity else ""}(
             t_idx += 1
             if t_idx > t_idx_max: break
         if t_idx > t_idx_max: break
-        %for p in ode_interpolated_parameters
-        while {p}_idx < {p}_idx_max and t > {p}_ts[{p}_idx+1]:
-            {p}_idx += 1
-        if {p}_idx == -1:
-            {p} = {p}_vals[0]
-        elif {p}_idx == {p}_idx_max:
-            {p} = {p}_vals[{p}_idx_max]
+        %for i in influences
+        while {i}_idx < {i}_idx_max and t > {i}_ts[{i}_idx+1]:
+            {i}_idx += 1
+        if {i}_idx == -1:
+            {i} = {i}_vals[0]
+        elif {i}_idx == {i}_idx_max:
+            {i} = {i}_vals[{i}_idx_max]
         else:
-            {p}_frac = (t - {p}_ts[{p}_idx])/({p}_ts[{p}_idx+1] - {p}_ts[{p}_idx])
-            {p} = {p}_frac*{p}_vals[{p}_idx+1] + (1-{p}_frac)*{p}_vals[{p}_idx]
+            {i}_frac = (t - {i}_ts[{i}_idx])/({i}_ts[{i}_idx+1] - {i}_ts[{i}_idx])
+            {i} = {i}_frac*{i}_vals[{i}_idx+1] + (1-{i}_frac)*{i}_vals[{i}_idx]
         %end
-        %for v in ode_variables
-        ddt_{v} = {ode_derivatives[v]}
+        %for v in variables
+        ddt_{v} = {dydt[v]}
         %end
 %if include_sensitivity
         # Calculate ddt_dydp:
@@ -163,38 +166,38 @@ def {ode_name}_integrate{"_with_sensitivity" if include_sensitivity else ""}(
         for v in range({num_vars}):
             for p in range(param_count):
                 ddt_dydp[v,p] = 0
-        %for v in range(num_vars)
+        %for v,v_s in enumerate(variables)
 
-        # ddt_dydp: Calculations for {ode_variables[v]}:
+        # ddt_dydp: Calculations for {v_s}
         #  - Paths through previous time step variables:
-        %   for v2 in range(num_vars)
-        %       if ddy_dydt[v][v2] != 0
-        ddy_dydt_val = {ddy_dydt[v][v2]}
-        for p in range(param_count): ddt_dydp[{v},p] += ddy_dydt_val*dydp[{v2},p]
+        %   for w,w_s in enumerate(variables)
+        %       if ddy_dydt[v][w] != 0
+        ddy_dydt_val = {ddy_dydt[v][w]}  # d^2({v_s})/(dt*d({w_s}))
+        for p in range(param_count): ddt_dydp[{v},p] += ddy_dydt_val*dydp[{w},p]
         %       end
         %   end
         #  - Paths through fixed parameters:
-        %   for fp in range(num_fparams)
-        %       if ddfp_dydt[v][fp] != 0
-        ddt_dydp[{v},{num_vars+fp}] += {ddfp_dydt[v][fp]}
+        %   for p,p_s in enumerate(parameters)
+        %       if ddp_dydt[v][p] != 0
+        ddt_dydp[{v},{num_vars+p}] += {ddp_dydt[v][p]}  # d^2({v_s})/(dt*d({p_s}))
         %       end
         %   end
         #  - Paths through interpolated paramters:
-        %   for ip,ip_s in enumerate(ode_interpolated_parameters)
-        %       if ddip_dydt[v][ip] != 0
-        ddip_dydp_val = {ddip_dydt[v][ip]}
-        if {ip_s}_idx == -1:
-            ddt_dydp[{v},{ip_s}_offset] += ddip_dydp_val
-        elif {ip_s}_idx == {ip_s}_idx_max:
-            ddt_dydp[{v},{ip_s}_offset+{ip_s}_idx_max] += ddip_dydp_val
+        %   for i,i_s in enumerate(influences)
+        %       if ddi_dydt[v][i] != 0
+        ddi_dydp_val = {ddi_dydt[v][i]}  # d^2({v_s})/(dt*d({i_s}))
+        if {i_s}_idx == -1:
+            ddt_dydp[{v},{i_s}_offset] += ddi_dydp_val
+        elif {i_s}_idx == {i_s}_idx_max:
+            ddt_dydp[{v},{i_s}_offset+{i_s}_idx_max] += ddi_dydp_val
         else:
-            ddt_dydp[{v},{ip_s}_offset+{ip_s}_idx] += ddip_dydp_val*(1-{ip_s}_frac)
-            ddt_dydp[{v},{ip_s}_offset+{ip_s}_idx+1] += ddip_dydp_val*{ip_s}_frac
+            ddt_dydp[{v},{i_s}_offset+{i_s}_idx] += ddi_dydp_val*(1-{i_s}_frac)
+            ddt_dydp[{v},{i_s}_offset+{i_s}_idx+1] += ddi_dydp_val*{i_s}_frac
         %       end
         %   end
         %end
 %end
-        %for v in ode_variables
+        %for v in variables
         {v} += step*ddt_{v}
         %end
 %if include_sensitivity
@@ -213,16 +216,16 @@ def {ode_name}_integrate{"_with_sensitivity" if include_sensitivity else ""}(
 
 # {'-'*60}
 
-class {ode_name}_theano_op(tt.Op):
+class {name}_theano_op(tt.Op):
     \"\"\"
-    Run the augmented seir ODE in the theano graph.
+    Run the {name} ODE in the theano graph.
 
     Parameters
     ----------
-    %for ip in ode_interpolated_parameters
-    {ip}_ts: array
-        Array of times at which the {ip} parameter will be specified.
-        ({ip} is linearly interpolated between these times.)
+    %for i in influences
+    {i}_ts: array
+        Array of times at which the {i} parameter will be specified.
+        Must be ascending. ({i} is linearly interpolated between these times.)
     %end
     ts : array
         Array of times at which to evaluate the solution.
@@ -233,8 +236,8 @@ class {ode_name}_theano_op(tt.Op):
     itypes = [
             tt.TensorType(floatX, (False,)),  # y0, float vector
             tt.TensorType(floatX, (False,)),  # params, float vector
-            %for ip in ode_interpolated_parameters
-            tt.TensorType(floatX, (False,)),  # {ip}_vals, float vector
+            %for i in influences
+            tt.TensorType(floatX, (False,)),  # {i}_vals, float vector
             %end
     ]
     otypes = [
@@ -242,70 +245,70 @@ class {ode_name}_theano_op(tt.Op):
     ]
     def __init__(
             self,
-            %for ip in ode_interpolated_parameters
-            {ip}_ts,
+            %for i in influences
+            {i}_ts,
             %end
             ts,
             step):
-        %for ip in ode_interpolated_parameters
-        self.{ip}_ts = np.array({ip}_ts, dtype=float)
+        %for i in influences
+        self.{i}_ts = np.array({i}_ts, dtype=float)
         %end
         self.ts = np.array(ts, dtype=float)
         self.step = step
         self.n_times = len(ts)
-        self.n_states = {ode_name}_num_states
-        self.n_fixed_params = {ode_name}_num_fixed_params
-        param_chunks = [self.n_states, self.n_fixed_params, {ip_ts_lens}]
+        self.n_variables = {name}_num_variables
+        self.n_params = {name}_num_parameters
+        param_chunks = [self.n_variables, self.n_params, {influences_len_list}]
         self.chunk_boundaries = np.cumsum(param_chunks[:-1])
 
     def perform(self, node, inputs, outputs):
-        y0, params, {vals_ip_vars} = inputs
-        outputs[0][0] = {ode_name}_integrate(
-                y0, params, {all_ip_variables} self.ts, self.step)
+        y0, params, {influences_vals_vars} = inputs
+        outputs[0][0] = {name}_integrate(
+                y0, params, {all_influences_vars} self.ts, self.step)
 
     def grad(self, inputs, g):
-        y0, params, {vals_ip_vars} = inputs
-        output, sensitivity = {ode_name}_integrate_with_sensitivity(
-                y0, params, {all_ip_variables} self.ts, self.step)
+        y0, params, {influences_vals_vars} = inputs
+        output, sensitivity = {name}_integrate_with_sensitivity(
+                y0, params, {all_influences_vars} self.ts, self.step)
         inputs_g = np.tensordot(g, sensitivity, axes=([0,1], [0,1]))
         return np.split(inputs_g, self.chunk_boundaries)
 
     def infer_shape(self, node, input_shapes):
-        return [(self.n_times, self.n_states)]
+        return [(self.n_times, self.n_variables)]
 """, globals(), locals())
     cython_output.write(integrator_code)
 
 
 
 ode_to_cython(
-        ode_name="pendulum",
-        ode_variables=["theta", "omega"],
-        ode_fixed_parameters = ["b", "c"],
-        ode_derivatives={
+        name="pendulum",
+        variables=["theta", "omega"],
+        parameters = ["b", "c"],
+        derivatives={
             "theta": "omega",
-            "omega": "-b*omega + c*sin(theta)"})
+            "omega": "-b*omega - c*sin(theta)"})
 
 ode_to_cython(
-        ode_name="sir",
-        ode_variables=["S", "I", "R"],
-        ode_fixed_parameters=["gamma"],
-        ode_interpolated_parameters=["beta"],
-        ode_derivatives={
+        name="sir",
+        variables=["S", "I", "R"],
+        parameters=["gamma"],
+        influences=["beta"],
+        derivatives={
             "S": "-(S*I/(S+I+R))*beta",
             "I": "(S*I/(S+I+R))*beta - gamma * I",
             "R": "gamma * I"})
 
 ode_to_cython(
-        ode_name="augmented_seir",
-        ode_variables=["S", "E", "I", "H", "D", "R"],
-        ode_fixed_parameters=[
+        name="augmented_seir",
+        variables=["S", "E", "I", "H", "D", "R"],
+        parameters=[
             "exposed_leave_rate",
             "infectious_leave_rate",
             "hospital_leave_rate",
             "hospital_p",
             "death_p"],
-        ode_interpolated_parameters=["beta"],
-        ode_derivatives={
+        influences=["beta"],
+        derivatives={
             "S": "-I*beta*S/(S+E+I+H+R)",
             "E": "I*beta*S/(S+E+I+H+R) - E*exposed_leave_rate",
             "I": "E*exposed_leave_rate - I*infectious_leave_rate",
