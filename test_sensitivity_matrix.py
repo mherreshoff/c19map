aedd23b27355207c3197b5e237d80f3516c92d69
Marcello Herreshoff <marcello.herreshoff@gmail.com>
2020-05-09 05:13:39 -0700

    Added another sensitivity matrix test.

--- test_sensitivity_matrix.py
+++ test_sensitivity_matrix.py
@@ -4,35 +4,45 @@ import matplotlib.pyplot as plt
 
 import integrators
 
-for i in range(1, 9):
-    plt.subplot(2, 4, i)
-    in_idx = (i-1) % 4
-    out_idx = (i-1) // 4
+# Influences not yet supported.
+def plot_sensitivities(name, y0, params, ts, step):
+    def get(s): return integrators.__dict__[f'{name}_{s}']
+    integ = get('integrate')
+    integ_with_sens = get('integrate_with_sensitivity')
+    variables = get('variables')
+    input_names = [v+"_0" for v in variables] + get('parameters')
+    output_names = get('variables')
 
-    in_var = ([s+"_0" for s in integrators.pendulum_variables] + integrators.pendulum_parameters)[in_idx]
-    out_var = integrators.pendulum_variables[out_idx]
+    num_inputs = len(input_names)
+    num_outputs = len(output_names)
 
-    b = 0.25
-    c = 5.0
+    for i in range(num_inputs*num_outputs):
+        plt.subplot(num_outputs, num_inputs, i+1)
+        out_idx, in_idx = divmod(i, num_inputs)
 
-    y0 = np.array([np.pi -0.1, 0.0], dtype=float)
-    params = np.array([b, c])
-    ts = np.linspace(0,10,101)
+        in_var = input_names[in_idx]
+        out_var = output_names[out_idx]
 
-    trajectory, sensitivity = integrators.pendulum_integrate_with_sensitivity(y0, params, ts, 0.01)
+        y0_a = np.array(y0, dtype=float)
+        params_a = np.array(params, dtype=float)
+        ts_a = np.array(ts, dtype=float)
 
-    dv = 0.01
-    if in_idx < 2: y0[in_idx] += dv
-    else: params[in_idx - 2] += dv
-    trajectory2 = integrators.pendulum_integrate(y0, params, ts, 0.01)
+        trajectory, sensitivity = integ_with_sens(y0_a, params_a, ts_a, step)
 
-    empircal_derivative = (trajectory2[:,out_idx] - trajectory[:,out_idx]) / dv
-    calculated_derivative = sensitivity[:,out_idx,in_idx]
+        dv = 0.01
+        if in_idx < len(variables): y0_a[in_idx] += dv
+        else: params_a[in_idx - len(variables)] += dv
+        trajectory2 = integ(y0_a, params_a, ts_a, 0.01)
 
-    plt.title(f'd({out_var})/d({in_var})')
-    plt.plot(ts, empircal_derivative, label='empirical')
-    plt.plot(ts, calculated_derivative, label='calculated')
-    plt.legend(loc='best')
-    plt.xlabel('t')
-    plt.grid()
-plt.show()
+        empircal_derivative = (trajectory2[:,out_idx] - trajectory[:,out_idx]) / dv
+        calculated_derivative = sensitivity[:,out_idx,in_idx]
+
+        plt.title(f'd({out_var})/d({in_var})')
+        plt.plot(ts, empircal_derivative, label='empirical')
+        plt.plot(ts, calculated_derivative, label='calculated')
+        plt.legend(loc='best')
+        plt.grid()
+    plt.show()
+
+plot_sensitivities('compound', [1], [0.05], np.arange(100)/10, 0.01)
+plot_sensitivities('pendulum', [np.pi - 0.1, 0], [0.25, 5.0], np.arange(100)/10, 0.01)
